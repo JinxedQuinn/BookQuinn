3a2cde1fd2a584372ff492c634742df3b3ea20ba
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-03-08 10:55:27 +0100

    Initiating Header

---
+++ bookquinn/app/components/Header.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Header = () => {
+    return (
+        <header className='text-center'>
+            Book de Chloé Nicolini
+        </header>
+    );
+};
+
+export default Header;
