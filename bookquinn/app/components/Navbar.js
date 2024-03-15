979d22c51e60c5d9489fa5f9c6291ae42c76a6c3
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-03-15 16:34:17 +0100

    Mise à jour de la barre de navigation

--- bookquinn/app/components/Navbar.js
+++ bookquinn/app/components/Navbar.js
@@ -1,11 +1,11 @@
 const Navbar = () => {
     return (
         <nav className="flex justify-between p-2 bg-black">
-            <a href="#about" className="text-white hover:text-blue-500 border border-teal-600 rounded-full p-2">À propos</a>
-            <a href="#skills" className="text-white hover:text-pink-500 border border-teal-600 rounded-full p-2">Compétences</a>
-            <a href="#projects" className="text-white hover:text-white border border-teal-600 rounded-full p-2">Projets</a>
-            <a href="#cv" className="text-white hover:text-pink-500 border border-teal-600 rounded-full p-2">CV</a>
-            <a href="#contact" className="text-white hover:text-blue-500 border border-teal-600 rounded-full p-2">Contact</a>
+            <a href="#about" className="text-white hover:text-blue-500 p-2">À propos</a>
+            <a href="#skills" className="text-white hover:text-pink-500 rounded-full p-2">Compétences</a>
+            <a href="#projects" className="text-white hover:text-white rounded-full p-2">Projets</a>
+            <a href="#cv" className="text-white hover:text-pink-500 rounded-full p-2">CV</a>
+            <a href="#contact" className="text-white hover:text-blue-500 rounded-full p-2">Contact</a>
         </nav>
     );
 };
