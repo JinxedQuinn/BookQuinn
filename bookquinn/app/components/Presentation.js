132ddf252657123fb1f87899b577db76c4e5fb52
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-03-18 16:07:44 +0100

    Added Skills and Tools in SVG, Found h-screen

--- bookquinn/app/components/Presentation.js
+++ bookquinn/app/components/Presentation.js
@@ -2,16 +2,30 @@ import Link from 'next/link';
 
 const Presentation = () => {
     return (
-        <div>
-            Présentation
-            <div>
-                Mes stacks
-                <div className='grid-cols-4'>
-                    <img src='/javascript.svg' alt='Javascript' />
-                </div>
-            </div>
-            <div>
-                Mes outils
+        <div className='p-3 bg-gradient-to-r from-fuchsia-500 to-cyan-500 h-screen'>
+            <h1 className='text-center p-3 text-4xl font-bold'>
+                Chloé Nicolini, développeuse full stack
+            </h1>
+
+            <p className='text-center p-2 border border-indigo-800 rounded-lg shadow-lg'>
+                Junior en code, j'ai pour ambition de monter en compétences afin de développer un projet personnel que j'aimerais voir aboutir. Pour cela, j'ai décidé d'entamer une formation chez Ada Tech School pour devenir développeuse web.
+            </p>
+
+            <p className='p-2 text-center'>
+                Je suis à la recherche d'une alternance de <strong>12 mois</strong>, sur un rythme de <strong>4 jours/semaine en entreprise.</strong>
+            </p>
+            <div className='flex justify-between'>
+                <button className='flex max-w-sm rounded-xl bg-gradient-to-tr from-pink-300 to-blue-300 p-0.5 shadow-lg'>
+                    <button className="flex-1 font-bold text-xl bg-white px-6 py-3 rounded-xl">
+                        Formée à Ada Tech School
+                    </button>
+                </button>
+
+                <button className='flex max-w-sm rounded-xl bg-gradient-to-tr from-pink-300 to-blue-300 p-0.5 shadow-lg'>
+                    <button className="flex-1 font-bold text-xl bg-white px-6 py-3 rounded-xl">
+                        From Oct. 2023
+                    </button>
+                </button>
             </div>
         </div>
     );
--- bookquinn/app/components/Skills.js
+++ bookquinn/app/components/Skills.js
@@ -1,10 +1,33 @@
 import Link from 'next/link';
+import { FaGithub } from "react-icons/fa";
 
 const Skills = () => {
     return (
-        <header className='text-center bg-blue-500 p-2 text-2xl'>
-            Skills
-        </header>
+        <div className='flex justify-between p-4'>
+            <div>
+                <p className='text-center'>Mes stacks</p>
+                <div className='grid grid-cols-4 p-2 w-full'>
+                    <img className='w-12 m-2' src='/javascript.svg' alt='Javascript' />
+                    <img className='w-12 m-2' src='/html5.svg' alt='HTML' />
+                    <img className='w-12 m-2' src='/css3.svg' alt='CSS' />
+                    <img className='w-12 m-2' src='/tailwindcss.svg' alt='TailwindCSS' />
+                    <img className='w-12 m-2' src='/react.svg' alt='React' />
+                    <img className='w-12 m-2' src='/nextjs.svg' alt='Nextjs' />
+                    <img className='w-12 m-2' src='/sql.svg' alt='SQL' />
+                    <img className='w-12 m-2' src='/php.svg' alt='PHP' />
+                </div>
+            </div>
+            <div>
+                <p className='text-center'>Mes outils</p>
+                <div className='grid grid-cols-4 p-2 w-full'>
+                    <img className='w-12 m-2' src='/vscode.svg' alt='VS Code' />
+                    <img className='w-12 m-2' src='/github.svg' alt='GitHub' />
+                    <img className='w-12 m-2' src='/notion.svg' alt='Notion' />
+                    <img className='w-12 m-2' src='/discord.svg' alt='Discord' />
+                    <img className='w-12 m-2' src='/postman.svg' alt='PostMan' />
+                </div>
+            </div>
+        </div>
     );
 };
 
