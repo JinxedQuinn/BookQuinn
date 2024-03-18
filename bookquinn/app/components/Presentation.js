d08adc8bf802e10ef7adb3e442b69972d77e1ddd
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-03-18 12:13:06 +0100

    Started to put stacks

--- bookquinn/app/components/Presentation.js
+++ bookquinn/app/components/Presentation.js
@@ -4,6 +4,15 @@ const Presentation = () => {
     return (
         <div>
             Présentation
+            <div>
+                Mes stacks
+                <div className='grid-cols-4'>
+                    <img src='/javascript.svg' alt='Javascript' />
+                </div>
+            </div>
+            <div>
+                Mes outils
+            </div>
         </div>
     );
 };
