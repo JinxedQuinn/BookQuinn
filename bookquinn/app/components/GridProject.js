8b835905898310d7814976e3a2703c32fe95d596
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-03-21 16:50:42 +0100

    Mise en page de la grille et traduction en français

--- bookquinn/app/components/GridProject.js
+++ bookquinn/app/components/GridProject.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const GridProject = () => {
     return (
-        <div>
+        <section className='h-screen bg-gradient-to-r from-teal-600 to-pink-900'>
             <div className="container mx-auto">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 m-2">
                     <div className="bg-white shadow-lg rounded-lg overflow-hidden">
@@ -22,7 +22,7 @@ const GridProject = () => {
                     <div className="bg-white shadow-lg rounded-lg overflow-hidden">
                         <img src="AMPUNV.png" alt="" className="w-full h-56 object-cover object-center" />
                         <div className="p-4">
-                            <h1 className="text-gray-900 font-bold text-2xl">Project 2</h1>
+                            <h1 className="text-gray-900 font-bold text-2xl">Plateforme de vente de meuble</h1>
                             <p className="mt-2 text-gray-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
                             <a href="https://github.com/adatechschool/reseau-social-php-chacsocialnetwork" className="flex items-center mt-3 text-blue-500 hover:underline">
                                     Lien Réseau Social PHP
@@ -35,7 +35,7 @@ const GridProject = () => {
 
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
 
